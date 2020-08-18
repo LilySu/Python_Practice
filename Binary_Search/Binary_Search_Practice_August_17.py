da04a0bf57e4bb86eb75a34a9955018446f47e78
LilySu <LilySu@users.noreply.github.com>
2020-08-18 00:19:46 -0400

    added binary search practice

---
+++ Binary_Search/Binary_Search_Practice_August_17.py
@@ -0,0 +1,116 @@
+def BinarySearch(nums, target):
+    if len(nums) == 0:
+        return -1
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def BinarySearch(nums, target):
+    if len(nums) == 0:
+        return -1
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+
+
+
+
+
+
+
+def BinarySearch(nums, target):
+    if len(nums) == 0:
+        return -1
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+def BinarySearch(nums, target):
+    if len(nums) == 0:
+        return -1
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+
+
+
+
+
+
+def BinarySearch(nums, target):
+    if len(nums) == 0:
+        return -1
+    left = 0
+    right = len(nums) - 1
+    mid = (left + right) // 2
+    while left < right:
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
