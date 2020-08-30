dcd10e32bd29fa0db7f384ca256ea311a074ebb7
LilySu <LilySu@users.noreply.github.com>
2020-08-29 23:58:36 -0400

    adding some oop practice

--- OOP_Little_Exercises/Elevator.py
+++ OOP_Little_Exercises/Elevator.py
@@ -9,4 +9,36 @@
 # floor_height - height of the floor
 import argparse
 
+parser = argparse.ArgumentParser(description='Run Elevator')
+parser.add_argument('-e','--entered_floor', help='Floor coming from', type=int, metavar='', required=False)
+parser.add_argument('-f','--floor', help='Floor to go to', type=int, metavar='', required=False)
+args = parser.parse_args()
+
 class Elevator:
+    def __init__(self, current_floor=0):
+        self.current_floor = current_floor
+
+    def hardware_functions(self):
+        return "door open | sensor does not sense anything blocking door | door close"
+
+    def elevator_movement(self, enter_floor, button_press):
+        if enter_floor is None:
+            enter_floor = self.current_floor
+        led_display_panel = []
+        for floor in range(enter_floor, button_press + 1):
+            led_display_panel.append('ding ' + str(floor))
+        return led_display_panel
+
+    def go_to(self, enter_floor = 0 , button_press = 0):
+        result = []
+        hardware_response_start = self.hardware_functions()
+        sound = self.elevator_movement(enter_floor, button_press)
+        hardware_response_end = self.hardware_functions()
+        result.append(hardware_response_start)
+        result.append(sound)
+        result.append(hardware_response_end)
+        return result
+
+if __name__ == '__main__':
+    e = Elevator()
+    print(e.go_to(args.entered_floor, args.floor))
---
+++ OOP_Little_Exercises/Elevator_Detail.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    input = input("Please enter a number ")
+    print(str(input) + " ding")
