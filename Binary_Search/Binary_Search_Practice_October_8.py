7902f43fe6509bc16b28e7607cd71a810593963d
LilySu <LilySu@users.noreply.github.com>
2020-10-08 23:51:47 -0400

    binary search recitation

---
+++ Binary_Search/Binary_Search_Practice_October_8.py
@@ -0,0 +1,59 @@
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1 
+
+
+if __name__ == '__main__':
+    nums = [2,4,6,7,9,12,15,18]
+    target = 15
+    print(binary_search(nums, target))
