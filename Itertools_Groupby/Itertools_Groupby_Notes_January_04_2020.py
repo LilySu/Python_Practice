5968b257c5a227201396e9783e72905c7169b810
LilySu <LilySu@users.noreply.github.com>
2021-01-04 23:01:06 -0500

    added groupby wip

---
+++ Itertools_Groupby/Itertools_Groupby_Notes_January_04_2020.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+
+elements = [100, 80, 80, 80, 80, 90, 90, 85, 85, 85]
+
+new_list = []
+# for i in groupby(elements):
+#     new_list.append(i[0])
+
+new_list = [i[0] for i in groupby(elements)]
+
+from operator import itemgetter
+new_mapped = map(itemgetter(0), groupby(elements))
+
+
+print(new_list)
+print(new_mapped)
