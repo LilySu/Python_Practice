08eb367f0aa4d46ee714d2474d7432be5a29d9d5
LilySu <LilySu@users.noreply.github.com>
2021-01-18 23:58:26 -0500

    added recall of itertools and added template

---
+++ Itertools_Groupby/Itertools_Groupby_Template_2021.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+
+s = [1,1,1,2,2,3,4,4,4]
+print()
+
+from itertools import product
+
+sublists = [[1,2,3],[4,5,6],[7,8,9]]
+print()
+
+not_all_sublists =  [[1,2,3],[4,5,6],[7,8,9],10,11]
+
+from itertools import chain, combinations
+
+def powerset(iteratble):
+    pass
