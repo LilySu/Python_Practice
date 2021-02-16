da93cbceea8708cbb53820f3cef4620b876e3b04
LilySu <LilySu@users.noreply.github.com>
2021-02-15 23:22:25 -0500

    added datacamp notes

--- sql/DataCamp_Storage/intermediate_04_CTE.sql
+++ sql/DataCamp_Storage/intermediate_04_CTE.sql
@@ -13,3 +13,74 @@ FROM league AS l
 -- Join the CTE to the league table
 LEFT JOIN match_list ON match_list.country_id
 GROUP BY l.name;
+
+
+
+
+WITH match_list AS (
+  -- Select the league, date, home, and away goals
+    SELECT
+  		l.name AS league,
+     	date,
+  		m.home_goal,
+  		m.away_goal,
+       (m.home_goal + m.away_goal) AS total_goals
+    FROM match AS m
+    LEFT JOIN league as l ON m.country_id = l.id)
+-- Select the league, date, home, and away goals from the CTE
+SELECT league, date, home_goal, away_goal
+FROM match_list
+-- Filter by total goals
+WHERE total_goals >= 10;
+
+
+
+
+-- Set up your CTE
+WITH match_list AS (
+    SELECT
+  		country_id,
+  	   (home_goal + away_goal) AS goals
+    FROM match
+  	-- Create a list of match IDs to filter data in the CTE
+    WHERE id IN (
+       SELECT id
+       FROM match
+       WHERE season = '2013/2014' AND EXTRACT(MONTH FROM date) = 8))
+-- Select the league name and average of goals in the CTE
+SELECT
+	name,
+    AVG(goals)
+FROM league AS l
+-- Join the CTE onto the league table
+LEFT JOIN match_list ON l.id = match_list.country_id
+GROUP BY l.name;
+
+
+
+
+SELECT
+	m.date,
+    -- Get the home and away team names
+    hometeam,
+    awayteam,
+    m.home_goal,
+    m.away_goal
+FROM match AS m
+
+-- Join the home subquery to the match table
+LEFT JOIN (
+  SELECT match.id, team.team_long_name AS hometeam
+  FROM match
+  LEFT JOIN team
+  ON match.hometeam_id = team.team_api_id) AS home
+ON home.id = m.id
+
+-- Join the away subquery to the match table
+LEFT JOIN (
+  SELECT match.id, team.team_long_name AS awayteam
+  FROM match
+  LEFT JOIN team
+  -- Get the away team ID in the subquery
+  ON match.awayteam_id = team.team_api_id) AS away
+ON away.id = m.id;
