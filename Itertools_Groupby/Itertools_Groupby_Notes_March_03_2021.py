5fc2554364ab6e6af53b797b30b0c241b25c17b1
LilySu <LilySu@users.noreply.github.com>
2021-03-03 23:07:05 -0500

    added spaced repetition itertools

---
+++ Itertools_Groupby/Itertools_Groupby_Notes_March_03_2021.py
@@ -0,0 +1,19 @@
+from itertools import groupby
+
+s = [1,1,1,2,2,3,4,4,4]
+print([i[0] for i in groupby(s)])
+
+from itertools import product
+
+sublists = [[1,2,3],[4,5,6],[7,8,9]]
+print(list(product(*sublists)))
+
+not_all_sublists =  [[1,2,3],[4,5,6],[7,8,9],10,11]
+
+from itertools import chain, combinations
+
+def powerset(iterable):
+    a = list(iterable)
+    return chain.from_iterable(combinations(a, i) for i in range(len(a) +1))
+
+print([i for i in powerset(sublists)])
